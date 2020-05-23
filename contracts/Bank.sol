e865f72694a7dc2cd4cb4101e156839f3bba6077
Joby Augustine <printfjoby@gmail.com>
2020-05-23 17:52:47 +0530

    Updated Function getLoanDurationAndIntrest()

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -737,11 +737,17 @@ contract Bank is Ownable {
     /**
      * @notice Get all loan durations and their interest rates
      * @dev Get all loan durations and their interest rates.
-     * @param _duration Loan duration.
-     * @param _interest Loan interest.
+     * @param _tariffId Loan tarrif Id.
+     * @return _duration Loan duration.
+     * @return _interest Loan interest.
      */
-    function getLoanDurationAndInterest() external returns(uint256 _duration, uint256  _interest) {
-        
+    function getLoanDurationAndInterest(uint256 _tariffId) external view returns(uint256 _duration, uint256  _interest) {
+        for(uint256 i = 0; i < lnTariff.length; i++){
+            if(lnTariff[i].tariffId == _tariffId){
+                _duration = lnTariff[i].duration;
+                _interest = lnTariff[i].interest;
+            }
+        }
     }
     
     /**
